2c5d32b701b831266516b9d5534d7c82ed9507d0
Kuntal Beniwal <kuntalbeniwal@192.168.1.9>
2024-04-17 13:15:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const APIkey = "17d815bed61b4eacbba31af1cf4b24d8";
+const contentlimit = 18;
+const url = "https://newsapi.org/v2/everything?q=";
+window.addEventListener("load", () => fetchNews("India"));
+
+function reload() {
+  window.location.reload();
+}
+
+async function fetchNews(query) {
+  const res = await fetch(`${url}${query}&apiKey=${APIkey}&PageSize=${contentlimit}`);
+  const data = await res.json();
+  bindData(data.articles);
+}
+
+function bindData(articles) {
+  const cardsContainer = document.getElementById("cards-container");
+  const newsCardTemplate = document.getElementById("template-news-card");
+
+  cardsContainer.innerHTML = "";
+
+  articles.forEach((article) => {
+    if (!article.urlToImage) return;
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+    fillDataInCard(cardClone, article);
+    cardsContainer.appendChild(cardClone);
+  });
+}
+
+function fillDataInCard(cardClone, article) {
+  const newsImg = cardClone.querySelector("#news-img");
+  const newsTitle = cardClone.querySelector("#news-title");
+  const newsSource = cardClone.querySelector("#news-source");
+  const newsDesc = cardClone.querySelector("#news-desc");
+
+  newsImg.src = article.urlToImage;
+  newsTitle.innerHTML = article.title;
+  newsDesc.innerHTML = article.description;
+
+  const date = new Date(article.publishedAt).toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta",
+  });
+
+  newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+  cardClone.firstElementChild.addEventListener("click", () => {
+    window.open(article.url, "_blank");
+  });
+}
+
+
+function onNavItemClick(id) {  //function on click defined for nav items
+  fetchNews(id);
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {  //function for search box
+  const query = searchText.value;
+  if (!query) return;
+  fetchNews(query);
+  ;
+});
